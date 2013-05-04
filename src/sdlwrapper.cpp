cd982f4bf9f46255e608cba481c6406215d55a2d
kadensotek <bobby23jones@gmail.com>
2013-05-03 22:47:58 -0400

    Added variables

--- src/sdlwrapper.cpp
+++ src/sdlwrapper.cpp
@@ -7,3 +7,19 @@
 #include <map>
 #include <iostream>
 #include <fstream>
+
+namespace sdlwrapper
+{
+
+    /* Variables */
+    int w; //screen width
+    int h; //screen height
+
+    std::map<int, bool> keypressed; //if key is pressed and released
+    SDL_Surface* scr; //SDL surface
+    Uint8* inkeys;
+    SDL_Event event = {0};
+
+
+
+}
