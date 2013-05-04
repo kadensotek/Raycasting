52dc46a863f24ed0eff7b4c6139c0c41428ccc1d
kadensotek <bobby23jones@gmail.com>
2013-05-03 22:51:22 -0400

    Added keyboard functions

--- src/sdlwrapper.cpp
+++ src/sdlwrapper.cpp
@@ -21,5 +21,34 @@ namespace sdlwrapper
     SDL_Event event = {0};
 
 
+    /* Keyboard functions */
+    bool keyDown(int key) /* returns true while key is held down */
+    {
+        return (inkeys[key] != 0);
+    }
+
+    bool keyPressed(int key) //returns true only once until key is released
+    {
+        if(keypressed.find(key) == keypressed.end())
+        {
+            keypressed[key] = false;
+        }
+
+        if(inkeys[key])
+        {
+            if(keypressed[key] == false)
+            {
+                keypressed[key] = true;
+                return true;
+            }
+        }
+        else
+        {
+            keypressed[key] = false;
+        }
+
+        return false;
+    }
+
 
 }
