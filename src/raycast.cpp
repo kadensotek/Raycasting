7e23b3d04046a742d7b21d44c2a7a06598987474
kadensotek <bobby23jones@gmail.com>
2013-05-08 21:41:10 -0400

    Added definitions

--- src/raycast.cpp
+++ src/raycast.cpp
@@ -5,6 +5,16 @@
 
 #include "sdlwrapper.h"
 
+using namespace sdlwrapper;
+
+#define screenWidth 640
+#define screenHeight 480
+#define texWidth 64
+#define texHeight 64
+#define mapWidth 24
+#define mapHeight 24
+
+
 int main()
 {
     return 0;
