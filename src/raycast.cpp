cc67bd61c2f61e53820dcb013086a1c9114c012d
kadensotek <bobby23jones@gmail.com>
2013-07-03 15:35:29 -0400

    Graphical rendering finished

--- src/raycast.cpp
+++ src/raycast.cpp
@@ -15,17 +15,17 @@ using namespace SDLwrapper;
 
 int worldMap[mapWidth][mapHeight]=
 {
+    {1,1,1,1,1,1,1,1,1,1},
+    {1,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,1},
     {1,1,1,1,1,1,1,1,1,1}
-    {1,0,0,0,0,0,0,0,0,1}
-    {1,0,0,0,0,0,0,0,0,1}
-    {1,0,0,0,0,0,0,0,0,1}
-    {1,0,0,0,0,0,0,0,0,1}
-    {1,0,0,0,0,0,0,0,0,1}
-    {1,0,0,0,0,0,0,0,0,1}
-    {1,0,0,0,0,0,0,0,0,1}
-    {1,0,0,0,0,0,0,0,0,1}
-    {1,1,1,1,1,1,1,1,1,1}
-}
+};
 
 int main()
 {
@@ -43,23 +43,128 @@ int main()
 
     while(!done())
     {
-        /* ray position and direction */
-        double cameraX = 2*x / double(w)-1;
-        double rayPosX = posX;
-        double rayPosY = posY;
-        double rayDirX = dirX + planeX * cameraX;
-        double rayDirY = dirY + planeY * cameraX;
-        int mapX = int(rayPosX);
-        int mapY = int(rayPosY);
+        for(int x=0; x<w; x++)
+        {
+            /* ray position and direction */
+            double cameraX = 2*x / double(w)-1;
+            double rayPosX = posX;
+            double rayPosY = posY;
+            double rayDirX = dirX + planeX * cameraX;
+            double rayDirY = dirY + planeY * cameraX;
+            int mapX = int(rayPosX);
+            int mapY = int(rayPosY);
 
-        /* used to figure out how far next X or Y side is from current position */
-        double sideDistX;
-        double sideDistY;
+            /* used to figure out how far next X or Y side is from current position */
+            double sideDistX;
+            double sideDistY;
 
-        /* length of ray between one X or Y side to next X or Y side */
-        double deltaDistX = sqrt(1 + (rayDirY * rayDirY) / (rayDirX * rayDirX));
-        double deltaDistY = sqrt(1 + (rayDirX * rayDirX) / (rayDirY * rayDirY));
-        double perpWallDist;
+            /* length of ray between one X or Y side to next X or Y side */
+            double deltaDistX = sqrt(1 + (rayDirY * rayDirY) / (rayDirX * rayDirX));
+            double deltaDistY = sqrt(1 + (rayDirX * rayDirX) / (rayDirY * rayDirY));
+            double perpWallDist;
+
+            /* what direction to step in x or y direction */
+            int stepX;
+            int stepY;
+
+            int hit = 0;
+            int side;  
+
+            /* calculate step and sideDist */
+            if(rayDirX<0)
+            {
+                stepX = -1;
+                sideDistX = (rayPosX - mapX) * deltaDistX;
+            }
+            else
+            {
+                stepX = 1;
+                sideDistX = (mapX + 1.0 - rayPosX) * deltaDistX;
+            }
+
+            if(rayDirY < 0)
+            {
+                stepY = -1;
+                sideDistY = (rayPosY - mapY) * deltaDistY;
+            }
+            else
+            {
+                stepY = 1;
+                sideDistY = (mapY + 1.0 - rayPosY) * deltaDistY;
+            }
+
+            /* digital differential analyzer algorithm */
+            while(hit == 0)
+            {
+                if(sideDistX < sideDistY)
+                {
+                    sideDistX += deltaDistX;
+                    mapX += stepX;
+                    side = 0;
+                }
+                else
+                {
+                    sideDistY += deltaDistY;
+                    mapY += stepY;
+                    side = 1;
+                }
+
+                /* does ray intersect wall? */
+                if(worldMap[mapX][mapY] > 0)
+                {
+                    hit = 1;
+                }
+            }
+
+            /* distance projected on camera direction */
+            if(side == 0)
+            {
+                perpWallDist = fabs((mapX - rayPosX + (1 - stepX) / 2) / rayDirX);
+            }
+            else
+            {
+                perpWallDist = fabs((mapY - rayPosY + (1 - stepY) / 2) / rayDirY);
+            }
+
+            /* calculate height of vertical line (gives illusion of distance) */
+            int lineHeight = abs(int(h/perpWallDist));
+
+            /* calculate start and end point of vertical line */
+            int drawStart = -lineHeight/2 + h/2;
+
+            if(drawStart < 0)
+            {
+                drawStart = 0;
+            }
+
+            int drawEnd = lineHeight/2 + h/2;
+
+            if(drawEnd >= h)
+            {
+                drawStart = h-1;
+            }
+
+            ColorRGB color;
+
+            switch(worldMap[mapX][mapY])
+            {
+                case 1:
+                       color = RGB_Red;
+                       break;
+                default:
+                       color = RGB_Yellow;
+                       break;
+            }
+
+            /* gives shadow to one side */
+            if(side == 1)
+            {
+               color = color/2;
+            }
+
+            /* draw the vertical line */
+            verLine(x, drawStart, drawEnd, color);
+        }
 
         /* handles timing/FPS */
         oldTime = time;
